faa4145ed078630adb9f1d2c5754267825ee3311
Thrigger <21341498+Thrigger@users.noreply.github.com>
2021-12-02 06:30:07 +0100

    Corrected the names of the files

--- 2021/python/d2_2.py
+++ 2021/python/d2_2.py
@@ -3,13 +3,15 @@ inputs = open("../inputs/d02.txt").read().strip().split("\n")
 
 hor = 0
 dep = 0
+aim = 0
 for inst in inputs:
     direc, steps = inst.split(" ")
 
     if direc == "forward":
         hor += int(steps)
+        dep += aim * int(steps)
     elif direc == "up":
-        dep -= int(steps)
+        aim -= int(steps)
     elif direc == "down":
-        dep += int(steps)
+        aim += int(steps)
 print(dep*hor)
