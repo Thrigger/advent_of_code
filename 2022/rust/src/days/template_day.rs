86907a59b76384c937d3fcf280ddc3bf9c7d183a
Thrigger <21341498+Thrigger@users.noreply.github.com>
2023-01-07 12:01:35 +0100

    Updated template to default to &str

--- 2022/rust/src/days/template_day.rs
+++ 2022/rust/src/days/template_day.rs
@@ -1,5 +1,5 @@
-//pub fn solve(part: u32, input: &Vec<&str>, sample: &Vec<&str>) -> i64 {
-pub fn solve(part: u32, input: &Vec<i64>, sample: &Vec<i64>) -> i64 {
+pub fn solve(part: u32, input: &Vec<&str>, sample: &Vec<&str>) -> i64 {
+//pub fn solve(part: u32, input: &Vec<i64>, sample: &Vec<i64>) -> i64 {
     assert_eq!(part1(&sample), 0, "Error, sample problem is not solved");
     //assert_eq!(part2(&sample), 0, "Error, sample problem is not solved");
 
@@ -10,15 +10,15 @@ pub fn solve(part: u32, input: &Vec<i64>, sample: &Vec<i64>) -> i64 {
     }
 }
 
-//fn part2(data: &Vec<&str>) -> i64 {
-fn part2(data: &Vec<i64>) -> i64 {
+fn part2(data: &Vec<&str>) -> i64 {
+//fn part2(data: &Vec<i64>) -> i64 {
     let mut total = 0;
     //data.iter().map(|i| fuel2(*i)).sum();
     total
 }
 
-//fn part1(data: &Vec<&str>) -> i64 {
-fn part1(data: &Vec<i64>) -> i64 {
+fn part1(data: &Vec<&str>) -> i64 {
+//fn part1(data: &Vec<i64>) -> i64 {
     let mut total = 0;
 
     for line in data {
