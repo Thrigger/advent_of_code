68e1013e8fde70bf94d602e7830338af7dfcf437
Thrigger <21341498+Thrigger@users.noreply.github.com>
2023-12-03 09:33:35 +0100

    Day 4 prep

---
+++ 2023/python/d4_2.py
@@ -0,0 +1,19 @@
+inputs = open("../inputs/examples/d04.txt").read().strip().split("\n")
+#inputs = open("../inputs/d04.txt").read().strip().split("\n")
+
+res = 0
+for line in inputs:
+    print(line)
+
+print(res)
+
+#              function     itterator
+#map(lambda x: int(x), line.split("\n"))
+#vec = line.split(" ")
+#vec = list(b)
+#vec.append(a)
+#len(vec)
+#sorted(vec)   <- small to big
+#print(vec)
+#print(vec[0])
+
