e8c935c4f7aa2e69b3cea178b7f10c23a29ad80d
Thrigger <21341498+Thrigger@users.noreply.github.com>
2023-12-04 19:15:32 +0100

    Day 4 python 2023

--- 2023/python/d4_1.py
+++ 2023/python/d4_1.py
@@ -2,22 +2,21 @@
 inputs = open("../inputs/d04.txt").read().strip().split("\n")
 
 res = 0
-multi= [1] * len(inputs)
-i = 0
 for line in inputs:
     _, data = line.split(": ")
     wins, my = data.split(" | ")
     wins = wins.split()
     my = my.split()
-    new = len(set(wins) & set(my))
+    overlaps = len(set(wins) & set(my))
+    
+    points = 1
+    if overlaps == 0:
+        points = 0
 
-    for j in range(new):
-        new_index = i + j + 1
-        if new_index >= len(multi):
-            break
-        multi[new_index] += multi[i]
+    for i in range(overlaps - 1):
+        points *= 2
 
-    i += 1
+    res += points
 
-print(sum(multi))
+print(res)
 
