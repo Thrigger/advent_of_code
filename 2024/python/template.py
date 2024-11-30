cfd8afc6f01cb5c98c096cec0d92b17cc561004e
Thrigger <21341498+Thrigger@users.noreply.github.com>
2024-11-30 21:49:29 +0100

    Set up 2024

---
+++ 2024/python/template.py
@@ -0,0 +1,20 @@
+inputs = open("../inputs/examples/d01.txt").read().strip().split("\n")
+#inputs = open("../inputs/d01.txt").read().strip().split("\n")
+
+res = 0
+for line in inputs:
+    print(line)
+
+print(res)
+
+#              function     itterator
+#map(lambda x: int(x), line.split("\n"))
+#vec = line.split(" ")
+#vec = list(b)
+#vec.append(a)
+#len(vec)
+#sorted(vec)   <- small to big
+#print(vec)
+#print(vec[0])
+#list(set(some_list) & set(some_other_list))    <-- check overlapping
+
---
+++ 2024/rust/src/main.rs
@@ -0,0 +1,9 @@
+mod days;
+
+fn main() {
+    /* days::solve takes two argument, the first is the day and the second is the part */
+    let day = 1;
+    println!("--Day {}--",day);
+    println!("--Part 1--\n{}", days::solve(day,1));
+    println!("--Part 2--\n{}", days::solve(day,2));
+}
