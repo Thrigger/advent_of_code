3696ced2b5d8be1d4b79518ad238bdcfbfa94ec2
Thrigger <21341498+Thrigger@users.noreply.github.com>
2024-12-08 09:10:23 +0100

    2024 - day 8 python

---
+++ 2024/python/d8_1.py
@@ -0,0 +1,41 @@
+#inputs = open("../inputs/examples/d08.txt").read().strip().split("\n")
+inputs = open("../inputs/d08.txt").read().strip().split("\n")
+
+res = 0
+ant_map = []
+for line in inputs:
+    ant_map.append(list(line))
+
+def is_inbound(amap, x, y):
+    if y >= 0 and y < len(amap) and x >= 0 and x < len(amap[0]):
+        return True
+    return False
+
+def mark_antinodes(amap, x, y):
+    for j in range(y, len(amap)):
+        for i in range(len(amap[j])):
+            if j > y or i > x:
+                if amap[j][i][:1]  == amap[y][x][:1]:
+                    dx = x-i
+                    dy = y-j
+                    new_x = x + dx
+                    new_y = y + dy
+                    if is_inbound(amap, new_x, new_y):
+                        amap[new_y][new_x] += "#"
+                    new_x = i - dx
+                    new_y = j - dy
+                    if is_inbound(amap, new_x, new_y):
+                        amap[new_y][new_x] += "#"
+
+for j in range(len(ant_map)):
+    for i in range(len(ant_map[j])):
+        node = ant_map[j][i][:1] 
+        if node != ".":
+            mark_antinodes(ant_map, i, j)
+
+
+for line in ant_map:
+    for each in line:
+        if "#" in each:
+            res += 1
+print(res)
--- 2024/python/template.py
+++ 2024/python/template.py
@@ -1,5 +1,5 @@
-inputs = open("../inputs/examples/d01.txt").read().strip().split("\n")
-#inputs = open("../inputs/d01.txt").read().strip().split("\n")
+inputs = open("../inputs/examples/d08.txt").read().strip().split("\n")
+#inputs = open("../inputs/d08.txt").read().strip().split("\n")
 
 res = 0
 for line in inputs:
